e3580d353297ecc346739d0bf065661c11652758
Eric Wong <ewong@linkedin.com>
2015-10-21 10:34:03 -0700

    Fix UTC handling. Add basic tests.

--- parsetime.c
+++ parsetime.c
@@ -516,12 +516,6 @@ tod(struct tm *tm)
 
     if (sc_tokid == UTC) {
         /* Handle UTC offset */
-        hour += tm->tm_gmtoff / 3600;
-        hour %= 24;
-        minute += tm->tm_gmtoff / 60;
-        minute %= 60;
-        tm->tm_gmtoff = 0;
-        tm->tm_isdst = 0;
         utc_defined = 1;
         token();
     }
@@ -782,11 +776,12 @@ parsetime(int token_nr, char **token_arr, time_t *result)
     /* convert back to time_t
     */
 
+    runtime.tm_isdst = -1;
     runtimer = mktime(&runtime);
 
     if (utc_defined == 1) {
       // Remove gmtoff since mktime inserts it in
-      runtimer -= runtime.tm_gmtoff;
+      runtimer += runtime.tm_gmtoff;
     }
 
     if (runtimer < 0) {
--- parsetime_test.go
+++ parsetime_test.go
@@ -7,15 +7,32 @@ import (
 
 func TestParsetime(t *testing.T) {
 
-	f := "15:04:05 UTC January 02 2006"
-
-	n := time.Now().Truncate(time.Second)
-	d, err := Parsetime(n.Format(f))
+	f := "15:04:05 January 02 2006"
+	testDate := time.Now().Truncate(time.Second)
+	parseDate, err := Parsetime(testDate.Format(f))
 	if err != nil {
 		t.Fatal(err)
 	}
-	if d != n {
-		t.Errorf("Actual: %#v, expected: %#v\n", n, d)
+
+	if testDate != parseDate {
+		t.Errorf("Actual: %#v, expected: %#v\n", parseDate, testDate)
 	}
 
+	parseDate, err = Parsetime("18:00 UTC May 5 2003")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if parseDate.Unix() != 1052157600 {
+		t.Errorf("Actual %d, expected %d", parseDate.Unix(), 1052157600)
+	}
+
+	local, _ := time.LoadLocation("Local")
+	testDate = time.Date(2015, 12, 25, 10, 0, 0, 0, local)
+	parseDate, err = Parsetime(testDate.Format(f))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if testDate != parseDate {
+		t.Errorf("Actual: %#v, expected: %#v\n", parseDate, testDate)
+	}
 }
