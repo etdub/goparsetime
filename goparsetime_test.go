ea17b0ac3318bf5109db95d0d4fff9f647fa115c
Eric Wong <ewong@linkedin.com>
2016-03-15 10:39:35 -0700

    Split up unit tests

--- goparsetime_test.go
+++ goparsetime_test.go
@@ -6,8 +6,14 @@ import (
 	"time"
 )
 
-func TestParsetime(t *testing.T) {
+func testParsetimeInvalid(t *testing.T) {
+	_, err := Parsetime("invalid string")
+	if err == nil {
+		t.Fatal("Expected error")
+	}
+}
 
+func TestParsetimeDateFormat(t *testing.T) {
 	f := "15:04:05 January 02 2006"
 	testDate := time.Now().Truncate(time.Second)
 	parseDate, err := Parsetime(testDate.Format(f))
@@ -36,10 +42,14 @@ func TestParsetime(t *testing.T) {
 	if testDate != parseDate {
 		t.Errorf("Actual: %#v, expected: %#v\n", parseDate, testDate)
 	}
+}
 
+func TestParsetimeDayAddition(t *testing.T) {
 	// Date addition
+	f := "15:04:05 January 02 2006"
+	testDate := time.Now().Truncate(time.Second)
 	dateAdd := fmt.Sprintf("%s + 5 days", testDate.Format(f))
-	parseDate, err = Parsetime(dateAdd)
+	parseDate, err := Parsetime(dateAdd)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -47,10 +57,30 @@ func TestParsetime(t *testing.T) {
 	if expectedDate != parseDate {
 		t.Errorf("Actual: %#v, expected: %#v\n", parseDate, expectedDate)
 	}
+}
 
-	// Invalid date should return an error
-	parseDate, err = Parsetime("invalid string")
-	if err == nil {
-		t.Fatal("Expected error")
+func TestParsetimeMultipleAddition(t *testing.T) {
+	f := "15:04:05 January 02 2006"
+	testDate := time.Now().Truncate(time.Second)
+	dateAdd := fmt.Sprintf("%s + 5minutes - 10 seconds", testDate.Format(f))
+	parseDate, err := Parsetime(dateAdd)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expectedDate := testDate.Add(time.Duration(5 * time.Minute)).Add(time.Duration(-10 * time.Second))
+	if expectedDate != parseDate {
+		t.Errorf("Actual: %#v, expected: %#v\n", parseDate, expectedDate)
+	}
+}
+
+func TestParsetimeNow(t *testing.T) {
+	before := time.Now().Add(5 * time.Minute).Truncate(time.Second)
+	actual, err := Parsetime("now+5minute")
+	after := time.Now().Add(5 * time.Minute).Truncate(time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if before.After(actual) || after.Before(actual) {
+		t.Errorf("Actual: %#v, expected between %#v and %#v", actual, before, after)
 	}
 }
